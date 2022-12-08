ff7a5cf9fb039027a94f31dabef34521df508e70
Mashiro Toyooka <mashiro6@gmail.com>
2022-12-08 22:47:40 +0900

    others

--- flower/gpu_test.py
+++ flower/gpu_test.py
@@ -6,8 +6,10 @@ print(torch.cuda.is_available())
 
 if torch.cuda.is_available():
     print('使用できるGPUの数')
+#SBATCH --gres=gpu:2 とすると、torch.cuda.device_count() == 2になる。
     print(torch.cuda.device_count())
     print('デフォルトGPU番号')
     print(torch.cuda.current_device())
     print('環境変数 CUDA_VISIBLE_DEVICES')
     print(os.environ['CUDA_VISIBLE_DEVICES'])
+    print(type(os.environ['CUDA_VISIBLE_DEVICES']))
