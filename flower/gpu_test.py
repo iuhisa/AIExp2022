6bde4be834014c02a114bb46fdccb4ccf46ef604
Mashiro Toyooka <mashiro6@gmail.com>
2022-12-08 16:37:02 +0900

    環境変数のとこエラーでるから後に回す

--- flower/gpu_test.py
+++ flower/gpu_test.py
@@ -5,11 +5,9 @@ print('GPU 使用可能か')
 print(torch.cuda.is_available())
 
 if torch.cuda.is_available():
-    print('環境変数 CUDA_VISIBLE_DEVICES')
-    print(os.environ['CUDA_VISIBLE_DEVICES'])
-    
     print('使用できるGPUの数')
     print(torch.cuda.device_count())
-
     print('デフォルトGPU番号')
     print(torch.cuda.current_device())
+    print('環境変数 CUDA_VISIBLE_DEVICES')
+    print(os.environ['CUDA_VISIBLE_DEVICES'])
