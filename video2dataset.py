ca9def010d1f9c7ec5f5ea05f44c4108bdec7d23
Mashiro Toyooka <mashiro6@gmail.com>
2022-12-13 19:45:25 +0900

    仮(仮)

--- video2dataset.py
+++ video2dataset.py
@@ -9,6 +9,7 @@ from package.util import check_dir
 import numpy as np
 
 video_filename = 'mountain_video.mp4'
+target_size =  256
 
 dataset_name = video_filename.split('.')[0]
 dataset_path = osp.join('datasets', dataset_name)
@@ -24,8 +25,13 @@ img_names = []
 while True:
     ret, frame = cap.read()
     if not ret: break
-    img_name = 'img_{}.jpg'.format(str(i).zfill(3))
-    cv2.imwrite(osp.join(dataset_path, 'images', img_name), frame)
+    img_name = 'img_{}'.format(str(i).zfill(3))
+
+    h, w, _ = frame.shape
+    x = min(h, w) # shorter edge
+    cropped_frame = frame[h - x//2:h + x//2, w - x//2:w + x//2, :]
+    resized_frame = cv2.resize(cropped_frame, (target_size, target_size))
+    cv2.imwrite(osp.join(dataset_path, 'images', img_name + '.jpg'), resized_frame)
     img_names.append(img_name)
     i += 1
 cap.release()
