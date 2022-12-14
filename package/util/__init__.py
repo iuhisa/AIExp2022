db5b8761dfcfba6a4f5ad62b91237240581a3f22
iuhisa <iuhisa1271@gmail.com>
2022-12-15 02:27:52 +0900

    切り抜き用の関数追加

--- package/util/__init__.py
+++ package/util/__init__.py
@@ -5,6 +5,8 @@ import glob
 import os.path
 import os
 import torch.cuda as cuda
+import cv2
+import numpy as np
 
 def make_filepath_list(root = 'dataset'):
     train_dirpath = os.path.join(root, 'train')
@@ -29,3 +31,38 @@ def get_gpu_list():
     else:
         ret = []
     return ret
+
+
+def clip_image(file_path: str, h: int, w: int, type: str = 'center') -> np.ndarray:
+    img = cv2.imread(file_path)
+    h_, w_ = img.shape[:2]
+    top = bottom = left = right = 0
+    if h_ < h or w_ < w:
+        return None
+    if type == 'center':
+        if h_%2 == 0 and h%2 == 0:
+            top = h_/2-h/2
+            bottom = h_/2+h/2-1
+        elif h_%2 == 0 and h%2 != 0:
+            top = h_/2-int(h/2)-1
+            bottom = h_/2+int(h/2)-1
+        elif h_%2 != 0 and h%2 == 0:
+            top = int(h_/2)-h/2
+            bottom = int(h_/2)+h/2-1
+        else:
+            top = int(h_/2)-int(h/2)
+            bottom = int(h_/2)+int(h/2)
+
+        if w_%2 == 0 and w%2 == 0:
+            left = w_/2-w/2
+            right = w_/2+w/2-1
+        elif w_%2 == 0 and w%2 != 0:
+            left = w_/2-int(w/2)-1
+            right = w_/2+int(w/2)-1
+        elif w_%2 != 0 and w%2 == 0:
+            left = int(w_/2)-w/2
+            right = int(w_/2)+w/2-1
+        else:
+            left = int(w_/2)-int(w/2)
+            right = int(w_/2)+int(w/2)
+    return img[int(top): int(bottom)+1, int(left): int(right)+1]
