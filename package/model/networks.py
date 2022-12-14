c4690846d6cc372e372586f63c6e62972d2c4487
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2022-12-14 17:33:01 +0900

    multi gpu 試し

--- package/model/networks.py
+++ package/model/networks.py
@@ -67,6 +67,7 @@ def init_weights(net, init_type='normal', init_gain=0.02):
 def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     if len(gpu_ids) > 0:
         net.to(gpu_ids[0])
+        # net = torch.nn.parallel.DistributedDataParallel(net, gpu_ids) # もしかしたらエラーかも。
         net = torch.nn.DataParallel(net, gpu_ids)
     init_weights(net, init_type, init_gain=init_gain)
     return net
