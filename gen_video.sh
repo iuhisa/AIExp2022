1a56a3e8dd196aa6bc8085d223600de77604ffcb
mashi6 <mashiro6@gmail.com>
2022-12-16 00:50:59 +0900

    a

--- gen_video.sh
+++ gen_video.sh
@@ -5,4 +5,4 @@
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 gen_video.py --A_dataroot mountain_video --B_dataroot hoge --direction AtoB --load_size 256 --crop_size 256 --name 12-14_12-03-43 --model cycle_gan --batch_size 1 --epoch latest --netG resnet_9blocks
+python3 gen_video.py --A_dataroot mountain_video --B_dataroot hoge --direction AtoB --load_size 512 --crop_size 512 --name 12-14_17-02-36 --model cycle_gan --batch_size 1 --epoch latest --netG resnet_9blocks
