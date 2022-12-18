69236934d1fc52212991bbe22f3894bba68cd8b7
iuhisa <iuhisa1271@gmail.com>
2022-12-18 14:49:09 +0900

    改良

--- create_hokusai_hiroshige_dataset.py
+++ create_hokusai_hiroshige_dataset.py
@@ -17,21 +17,31 @@ def main():
     dir_path_hokusai = osp.join('datasets', 'katsushika_hokusai','images')
     check_dir(dir_path_hokusai)
     for i, file_path in enumerate(tqdm(os.listdir(osp.join(dir_path_ukiyoe_high_resolution, 'Katsushika_Hokusai')))):
-        img = clip_image(cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Katsushika_Hokusai', file_path)), 1000, 1000)
+        img = cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Katsushika_Hokusai', file_path))
+        h, w = img.shape[:2]
+        img = clip_image(img, min(h*0.8, w*0.8), min(h*0.8, w*0.8))
         if type(img)==np.ndarray:
+            img = cv2.resize(img, (1000, 1000))
             cv2.imwrite(osp.join(dir_path_hokusai, f'{i}.jpg'), img)
     
     print('utagawa_hiroshigeデータセットを作成します')
     dir_path_hiroshige = osp.join('datasets', 'utagawa_hiroshige','images')
     check_dir(dir_path_hiroshige)
     for i, file_path in enumerate(tqdm(os.listdir(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige')))):
-        img = clip_image(cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige', file_path)), 1000, 1000)
+        img = cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige', file_path))
+        h, w = img.shape[:2]
+        img = clip_image(img, min(h*0.8, w*0.8), min(h*0.8, w*0.8))
         if type(img)==np.ndarray:
+            img = cv2.resize(img, (1000, 1000))
             cv2.imwrite(osp.join(dir_path_hiroshige, f'{i}.jpg'), img)
+    
     for j, file_path in enumerate(tqdm(os.listdir(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige_II')))):
-        img = clip_image(cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige_II', file_path)), 1000, 1000)
+        img = cv2.imread(osp.join(dir_path_ukiyoe_high_resolution, 'Utagawa_Hiroshige_II', file_path))
+        h, w = img.shape[:2]
+        img = clip_image(img, min(h*0.8, w*0.8), min(h*0.8, w*0.8))
         if type(img)==np.ndarray:
-            cv2.imwrite(osp.join(dir_path_hiroshige, f'{i+j+1}.jpg'), img)
+            img = cv2.resize(img, (1000, 1000))
+            cv2.imwrite(osp.join(dir_path_hiroshige, f'{i}.jpg'), img)
 
 if __name__ == "__main__":
     main()
