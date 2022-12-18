702ea56d6ca498bd30a804d4491f2271e309c338
mashi6 <mashiro6@gmail.com>
2022-12-18 22:13:42 +0900

    ISTでの実行結果を追加

--- train_cyclegan.sh
+++ train_cyclegan.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH -p p
+#SBATCH -p v
 #SBATCH --gres=gpu:4
 
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train.py --A_dataroot photo_ukiyoe --B_dataroot hiroshige --load_size 286 --crop_size 256 --save_epoch_interval 10 --model cycle_gan --batch_size 16 --netG resnet_9blocks --num_threads 4 --pool_size 50 --n_epochs 300
+python3 train.py --A_dataroot fake_mask --B_dataroot real_mask2 --load_size 286 --crop_size 256 --save_epoch_interval 10 --model cycle_gan --batch_size 16 --batch_multiplier 4 --netG resnet_9blocks --num_threads 4 --pool_size 50
