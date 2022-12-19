308c6bc8bd6b424450ad0dddebb282ca663b1b58
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2022-12-20 04:48:01 +0900

    self attn 著者実装

--- package/model/networks.py
+++ package/model/networks.py
@@ -477,33 +477,35 @@ class PixelDiscriminator(nn.Module):
         return self.net(input)
 
 class Self_Attention(nn.Module):
-    def __init__(self, in_dim):
-        super(Self_Attention, self).__init__()
-
-        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
-        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
-        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
-
-        self.softmax = nn.Softmax(dim=-2)
-
+    """ Self attention Layer"""
+    def __init__(self,in_dim,activation):
+        super(Self_Attention,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         self.gamma = nn.Parameter(torch.zeros(1))
 
-    def forward(self, x):
-        X = x
-        proj_query = self.query_conv(X).view(X.shape[0], -1, X.shape[2]*X.shape[3])
-        proj_query = proj_query.permute(0, 2, 1)
-        proj_key = self.key_conv(X).view(X.shape[0], -1, X.shape[2]*X.shape[3])
+        self.softmax  = nn.Softmax(dim=-1) #
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
 
-        S = torch.bmm(proj_query, proj_key)
-
-        attention_map_T = self.softmax(S)
-        attention_map = attention_map_T.permute(0, 2, 1)
-
-        proj_value = self.value_conv(X).view(X.shape[0], -1, X.shape[2]*X.shape[3])
-        o = torch.bmm(proj_value, attention_map.permute(0, 2, 1))
-
-        o = o.view(X.shape[0], X.shape[1], X.shape[2], X.shape[3])
-        out = x + self.gamma*o
-
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(m_batchsize,C,width,height)
+        
+        out = self.gamma*out + x
         return out
-        # return out, attention_map
